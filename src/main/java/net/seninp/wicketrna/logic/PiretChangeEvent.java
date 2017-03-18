87434d30b7baf9a844f2f1cbb72e832084ab5f72
Pavel Senin <seninp@gmail.com>
2017-03-18 13:37:53 -0600

    commenting the code

--- src/main/java/net/seninp/wicketrna/logic/PiretChangeEvent.java
+++ src/main/java/net/seninp/wicketrna/logic/PiretChangeEvent.java
@@ -2,13 +2,23 @@ package net.seninp.wicketrna.logic;
 
 import java.util.EventObject;
 
+/**
+ * An utility class for passing events between components.
+ * 
+ * @author psenin
+ *
+ */
 public class PiretChangeEvent extends EventObject {
 
   private static final long serialVersionUID = 1L;
 
-  // This event definition is stateless but you could always
-  // add other information here.
+  /**
+   * This event definition is stateless.
+   * 
+   * @param source
+   */
   public PiretChangeEvent(Object source) {
     super(source);
   }
+
 }
--- src/main/java/net/seninp/wicketrna/logic/PiretChangeListener.java
+++ src/main/java/net/seninp/wicketrna/logic/PiretChangeListener.java
@@ -1,5 +1,16 @@
 package net.seninp.wicketrna.logic;
 
+/**
+ * An interface which takes care about passing events.
+ * 
+ * @author psenin
+ *
+ */
 public interface PiretChangeListener {
+  /**
+   * Event processing.
+   * 
+   * @param evt
+   */
   public void changeEventReceived(PiretChangeEvent evt);
 }
